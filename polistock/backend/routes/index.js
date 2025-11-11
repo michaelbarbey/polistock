c07f0999a7a7a02141745a56235bbd3eb9f09a79
Michael Schlichting Barbey <michael.schlichtingbarbey@baruchmail.cuny.edu>
2025-11-10 20:46:29 -0500

    ammended users.js

--- polistock/backend/routes/index.js
+++ polistock/backend/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+var router = Router();
 
 /* GET home page. */
 router.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the Express backend!' });
 });
 
-module.exports = router;
+export default router;

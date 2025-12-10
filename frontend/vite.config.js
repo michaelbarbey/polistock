1b5e972199783c5b8637a279e45d5b80f23eb679
Michael Schlichting Barbey <michael.schlichtingbarbey@baruchmail.cuny.edu>
2025-12-09 22:54:10 -0500

    importing frontend directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // Proxy API requests to your backend
-    },
-  },
-});
+      '/api': 'http://localhost:3000'
+    }
+  }
+})
